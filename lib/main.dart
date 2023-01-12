e14513708bd5f63199a7ba0acf1561c5b1e45fd7
RommelOjeda <56703120+RommelOjeda@users.noreply.github.com>
2023-01-12 14:09:13 -0500

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:contactos/screens/contactos_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:contactos/screens/notes_screen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,18 +16,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) => MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: title,
-    //themeMode: ThemeMode.dark,
-    theme: ThemeData(
-      primaryColor: Colors.black,
-      scaffoldBackgroundColor: Colors.blueGrey.shade900,
-      appBarTheme: AppBarTheme(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-    ),
-    home: NotesPage(),
-  );
+        debugShowCheckedModeBanner: false,
+        title: title,
+        themeMode: ThemeMode.dark,
+        theme: ThemeData(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.blueGrey.shade900,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+        ),
+        home: ContactosPage(),
+      );
 }
-
